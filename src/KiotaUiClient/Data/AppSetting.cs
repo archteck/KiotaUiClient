706e6a7902a8dc0143e5bc7fb66893c575901c94
archteck <david.rodrigues.91@outlook.com>
2025-09-21 17:06:22 +0100

    Add initial database context and migrations with Entity Framework Core

---
+++ src/KiotaUiClient/Data/AppSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace KiotaUiClient.Data;
+
+public class AppSetting
+{
+    public int Id { get; set; }
+    public string Key { get; set; } = default!;
+    public string? Value { get; set; }
+}
---
+++ src/KiotaUiClient/Migrations/20250921160524_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KiotaUiClient.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Key = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    Value = table.Column<string>(type: "TEXT", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppSettings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppSettings_Key",
+                table: "AppSettings",
+                column: "Key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppSettings");
+        }
+    }
+}
